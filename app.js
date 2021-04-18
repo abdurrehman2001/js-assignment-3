f237220ffbce5eb59797c90445f35d2af53d4185
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-04-19 03:23:25 +0500

    Js Three Questions Is Completed

---
+++ app.js
@@ -0,0 +1,43 @@
+var city = prompt("Search Your City.....???");
+
+if (city === "karachi"){
+    alert("Welcome To The City Of Light..")
+}
+else if (city === "punjab"){
+    alert("Welcome to Punjab")
+}
+else if (city === "sindh"){
+    alert("Welcome to sindh")
+}
+else if (city === "hyderabad"){
+    alert("Welcome to Hyderabad")
+}
+else{
+    alert("Sai se likho sare alfaz small me ese search kro.....\nkarachi\npunjab\nsindh\nhyderabad")
+}
+var gender = prompt("Type Your Genger");
+
+if(gender === "male"){
+    alert("Good Morning Sir..!")
+}
+else if (gender === "female"){
+    alert("Good Morning Maam..!")
+}
+else{
+    alert("Third Person Is Not Allowed...!!")
+}
+var color = prompt("Traffic Signal Colour Search Colour.........\nred\nyellow\ngreen");
+
+if (color === "red"){
+    alert("Red Light is show you MUST STOP IT....")
+}
+else if(color === "yellow"){
+    alert("Yellow Light is show You Must Ready To Move.....")
+}
+else if (color === "green"){
+    alert("Move Now your place..")
+}
+else{
+    alert("Signal nhi torna Chalan katega nhi to...")
+}
+
