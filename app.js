c5447562c85c42ff5c9e4391da7aaac4cdac1811
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-04-19 15:56:56 +0500

    Half

--- app.js
+++ app.js
@@ -41,3 +41,44 @@ else{
     alert("Signal nhi torna Chalan katega nhi to...")
 }
 
+var per = prompt("Search Your Result...")
+if (per >= 80){
+    alert("A-One Grade \n Excellent")
+}
+else if (per >= 70){
+    alert("A Grade \n Good")
+}
+else if (per >= 60){
+    alert("B Grade \n You need to improve")
+}
+else{
+    alert("Sorry..!!Your are Fail")
+}
+
+var time = prompt("Enter Timing...........")
+if(time > 13){
+    alert ("Good day")
+}
+else{
+    alert("Soory Time Is Finish")
+}
+
+var tem = prompt("Search Temperature")
+if (tem > 40){
+    alert("It is too hot outside")
+}
+else if ( tem > 30){
+    alert("The Weather today is Normal")
+}
+else if ( tem > 20){
+    alert("Today’s Weather is cool")
+}
+else if ( tem > 10){
+    alert("OMG! Today’s weather is so Cool")
+}
+else if (tem < 10){
+    alert("It is snowing")
+}
+else{
+    alert("Search Properly")
+}
